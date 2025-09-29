06409166de5c9f6ae42edaa74c8ec3149946395c
kerly suarez <kerly_suarezb@hotmail.com>
2025-09-28 22:23:19 -0500

    Diccionario informacion personal

---
+++ informacion_personal.py
@@ -0,0 +1,11 @@
+ 1. Crear el diccionario con información personal
+informacion_personal = {
+    "nombre": "Kerly Suárez",
+    "edad": 32,
+    "ciudad": "Guayaquil",
+    "profesion": "Estudiante de TIC"
+}
+
+print("Diccionario inicial:")
+print(informacion_personal)
+print()
